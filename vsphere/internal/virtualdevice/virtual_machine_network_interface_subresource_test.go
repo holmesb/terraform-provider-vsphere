1c9a01a1970b6a00e801cbdf49868138fae93ed9
holmesb <5072156+holmesb@users.noreply.github.com>
2023-05-24 18:38:36 +0100

    Workarund issue with PR 1417 whereby attributes that are only known at runtime (https://github.com/hashicorp/terraform-provider-vsphere/pull/1417#issuecomment-1561597019)

--- vsphere/internal/virtualdevice/virtual_machine_network_interface_subresource_test.go
+++ vsphere/internal/virtualdevice/virtual_machine_network_interface_subresource_test.go
@@ -6,112 +6,49 @@ package virtualdevice
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/structure"
-	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
-func TestNicUnitRange(t *testing.T) {
+func TestDiskCapacityInGiB(t *testing.T) {
 	cases := []struct {
 		name     string
-		devices  object.VirtualDeviceList
+		subject  *types.VirtualDisk
 		expected int
 	}{
 		{
-			name: "basic",
-			devices: object.VirtualDeviceList{
-				&types.VirtualVmxnet3{
-					VirtualVmxnet: types.VirtualVmxnet{
-						VirtualEthernetCard: types.VirtualEthernetCard{
-							VirtualDevice: types.VirtualDevice{
-								UnitNumber: structure.Int32Ptr(7),
-							},
-						},
-					},
-				},
-				&types.VirtualE1000{
-					VirtualEthernetCard: types.VirtualEthernetCard{
-						VirtualDevice: types.VirtualDevice{
-							UnitNumber: structure.Int32Ptr(8),
-						},
-					},
-				},
-				&types.VirtualE1000{
-					VirtualEthernetCard: types.VirtualEthernetCard{
-						VirtualDevice: types.VirtualDevice{
-							UnitNumber: structure.Int32Ptr(9),
-						},
-					},
-				},
-			},
-			expected: 3,
-		},
-		{
-			name: "single NIC not at first offset",
-			devices: object.VirtualDeviceList{
-				&types.VirtualVmxnet3{
-					VirtualVmxnet: types.VirtualVmxnet{
-						VirtualEthernetCard: types.VirtualEthernetCard{
-							VirtualDevice: types.VirtualDevice{
-								UnitNumber: structure.Int32Ptr(8),
-							},
-						},
-					},
-				},
-			},
-			expected: 2,
-		},
-		{
-			name: "hole in middle",
-			devices: object.VirtualDeviceList{
-				&types.VirtualVmxnet3{
-					VirtualVmxnet: types.VirtualVmxnet{
-						VirtualEthernetCard: types.VirtualEthernetCard{
-							VirtualDevice: types.VirtualDevice{
-								UnitNumber: structure.Int32Ptr(7),
-							},
-						},
-					},
-				},
-				&types.VirtualE1000{
-					VirtualEthernetCard: types.VirtualEthernetCard{
-						VirtualDevice: types.VirtualDevice{
-							UnitNumber: structure.Int32Ptr(9),
-						},
-					},
-				},
-			},
-			expected: 3,
-		},
-		{
-			name: "hole in middle and not starting at first offset",
-			devices: object.VirtualDeviceList{
-				&types.VirtualVmxnet3{
-					VirtualVmxnet: types.VirtualVmxnet{
-						VirtualEthernetCard: types.VirtualEthernetCard{
-							VirtualDevice: types.VirtualDevice{
-								UnitNumber: structure.Int32Ptr(8),
-							},
-						},
-					},
-				},
-				&types.VirtualE1000{
-					VirtualEthernetCard: types.VirtualEthernetCard{
-						VirtualDevice: types.VirtualDevice{
-							UnitNumber: structure.Int32Ptr(10),
-						},
-					},
-				},
+			name: "capacityInBytes - integer GiB",
+			subject: &types.VirtualDisk{
+				CapacityInBytes: 4294967296,
+				CapacityInKB:    4194304,
 			},
 			expected: 4,
+		},
+		{
+			name: "capacityInKB - integer GiB",
+			subject: &types.VirtualDisk{
+				CapacityInKB: 4194304,
+			},
+			expected: 4,
+		},
+		{
+			name: "capacityInBytes - non-integer GiB",
+			subject: &types.VirtualDisk{
+				CapacityInBytes: 4294968320,
+				CapacityInKB:    4194305,
+			},
+			expected: 5,
+		},
+		{
+			name: "capacityInKB - non-integer GiB",
+			subject: &types.VirtualDisk{
+				CapacityInKB: 4194305,
+			},
+			expected: 5,
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := nicUnitRange(tc.devices)
-			if err != nil {
-				t.Fatalf("bad: %s", err)
-			}
+			actual := diskCapacityInGiB(tc.subject)
 			if tc.expected != actual {
 				t.Fatalf("expected %d, got %d", tc.expected, actual)
 			}
